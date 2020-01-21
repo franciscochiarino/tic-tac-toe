8cf67938ed1ca30c12dc7e530ad296d3e1bbc067
fran <francisco.chiarino@gmail.com>
2020-01-21 13:14:22 +0100

    Tested stuff

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Get each div:
-const div1 = document.getElementById('1');
-const div2 = document.getElementById('2');
-const div3 = document.getElementById('3');
-const div4 = document.getElementById('4');
-const div5 = document.getElementById('5');
-const div6 = document.getElementById('6');
-const div7 = document.getElementById('7');
-const div8 = document.getElementById('8');
-const div9 = document.getElementById('9'); 
+// Get all the board into variables:
+const board = document.getElementById('board');
+const square1 = document.getElementById('square-1');
+const square2 = document.getElementById('square-2');
+const square3 = document.getElementById('square-3');
+const square4 = document.getElementById('square-4');
+const square5 = document.getElementById('square-5');
+const square6 = document.getElementById('square-6');
+const square7 = document.getElementById('square-7');
+const square8 = document.getElementById('square-8');
+const square9 = document.getElementById('square-9'); 
+
+// Try click
+
+square1.addEventListener('click', function() {
+    square1.style.backgroundColor = "blue";
+})
