2671ec8232ddedb8134a25c77b14ac5658d6bf20
Francisco Chiarino <francisco.chiarino@gmail.com>
2020-01-21 17:02:49 +0100

    Removed unnecessary code

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // Board Variables
 const board = document.getElementById('board');
 const squares = document.getElementsByClassName('square');
-const square1 = document.getElementById('square-1');
-const square2 = document.getElementById('square-2');
-const square3 = document.getElementById('square-3');
-const square4 = document.getElementById('square-4');
-const square5 = document.getElementById('square-5');
-const square6 = document.getElementById('square-6');
-const square7 = document.getElementById('square-7');
-const square8 = document.getElementById('square-8');
-const square9 = document.getElementById('square-9'); 
 
 // Combinations
 const basicCombinations = ['123', '456', '789', '147', '258', '369', '159', '753'];
