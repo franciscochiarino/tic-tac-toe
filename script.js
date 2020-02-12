425383cdead8fe6b3846f6e607e2a5a845ac640b
Francisco Chiarino <francisco.chiarino@gmail.com>
2020-02-12 22:45:45 +0100

    Ready to merge

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-
-let squares = document.getElementsByClassName('square');
-// Combinations
-const basicCombinations = ['123', '456', '789', '147', '258', '369', '159', '753'];
-const allPossibleCombinations = [];
-
+// If there is a winner, this variable is going to be reassigned to an empty string, so users can't keep playing
+let squares = document.getElementsByClassName('square')
 
 // Other Variables
-const display = document.getElementById('script-display');  // Where messages are going to be displayed
-let clicksInsideBoard = 0;  // Click counter
+const display = document.getElementById('script-display')  // Where messages are going to be displayed
+let clicksInsideBoard = 0  // Click counter
 
 // Create virtual board
 const virtualBoard = [
@@ -16,83 +12,82 @@ const virtualBoard = [
     ['', '', '']
 ]
 
-
-// Count the click inside the board, so we know who's who:
+// Count the clicks inside the board, so we know who's who
 const clickCounter = () => {
-    clicksInsideBoard += 1;
+    clicksInsideBoard += 1
 };
 
-// Check if someone wins:
+// Check if someone wins
 const findWinner = (array) => {
-    let counterP1 = 0;
-    let counterP2 = 0;
+    let counterP1 = 0
+    let counterP2 = 0
 
     // Using 'for' because forEach didn't work here
     for (let i = 0; i < array.length; i++) {
-        let sq = array[i];
-        (sq === 'X') ? counterP1 += 1 : false;
-        (sq === 'O') ? counterP2 += 1 : false;
+        let sq = array[i]
+        (sq === 'X') ? counterP1 += 1 : false
+        (sq === 'O') ? counterP2 += 1 : false
     }
 
     if (counterP1 === 3) {
         display.innerHTML = '<h3>Player One Wins!</h3>'
         confetti.start()
-        squares = '';
+        squares = ''
     }
     if (counterP2 === 3) {
         display.innerHTML = '<h3>Player Two Wins!</h3>'
         confetti.start()
-        squares = '';
+        squares = ''
     }
 }
 
-// Check all winning combinations
+// Get arrays
 const findCombinations = () => {
    
-    // Find combinations horizontally
+    // Horizontal arrays
     virtualBoard.forEach(row => {     
-        findWinner(row);
+        findWinner(row)
     })
 
-    // Find combinations verically
+    // Vertical arrays
     for (let i = 0; i < virtualBoard.length; i++) {
-        let col = [];
+        let col = []
 
         for (let j = 0; j < virtualBoard.length; j++) {
             col.push(virtualBoard[j][i])
         }
 
-        findWinner(col);
+        findWinner(col)
     }
 
-    // Find combinations diagonally
-    let diagonal1 = [virtualBoard[0][0], virtualBoard[1][1], virtualBoard[2][2]];
-    let diagonal2 = [virtualBoard[2][0], virtualBoard[1][1], virtualBoard[0][2]];
+    // Diagonal arrays
+    let diagonal1 = [virtualBoard[0][0], virtualBoard[1][1], virtualBoard[2][2]]
+    let diagonal2 = [virtualBoard[2][0], virtualBoard[1][1], virtualBoard[0][2]]
 
-    findWinner(diagonal1);
-    findWinner(diagonal2);
+    findWinner(diagonal1)
+    findWinner(diagonal2)
     
 } 
 
 // Draw & Locate clicks
 for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function() {
-        let squareId = squares[i].id;
+        let squareId = squares[i].id
         let squareRow = squareId[squareId.length - 2]
         let squareCol = squareId[squareId.length - 1]
         
         if (clicksInsideBoard % 2 === 0 && squares[i].innerHTML === '') {
-            squares[i].innerHTML = '<p>X</p>';
+            squares[i].innerHTML = '<p>X</p>'
             clickCounter()
             // Concatinate the last character of the square's id (it's number):
-            virtualBoard[squareRow][squareCol] = 'X';
+            virtualBoard[squareRow][squareCol] = 'X'
             // Check if player one won:
             findCombinations()
         } else if (squares[i].innerHTML === '') {
-            squares[i].innerHTML = '<p>O</p>';
+            squares[i].innerHTML = '<p>O</p>'
             clickCounter()
             // Concatinate the last character of the square's id (it's number):
-            virtualBoard[squareRow][squareCol] = 'O';
+            virtualBoard[squareRow][squareCol] = 'O'
             // Check if player two won:
             findCombinations()
         }
